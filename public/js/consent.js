79d72a6d7c2dc33073d62e756ddef26d68ac883e
Shahnur Islam <shahnur.islam@royalmail.com>
2018-12-20 14:33:21 +0000

    added cookie page

---
+++ public/js/consent.js
@@ -0,0 +1,137 @@
+//Geo locate users for conditional consent popup
+jQuery.get("https://freegeoip.net/json/", function (response) {
+    var country = response.country_code;
+    var euCountries = ['AT', 'BE', 'BG', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR', 'DE', 'GR', 'HU', 'HR', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL', 'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB'];
+    var inEU = euCountries.indexOf(country) !== -1;
+    if(inEU){
+		// Show GDPR compliant opt-in, opt-out cookie consent for all countries within the EU.
+
+		window.addEventListener("load", function(){
+		/*
+		 * deleteCookies
+		 * delete all cookies except those listed in the array essential
+		 */
+		 	window.cookieconsent.Popup.prototype.deleteCookies = function() {
+    			//List of essential cookies - set as an empty array to delete everything - i.e. var essential = [];
+    			var essential = ["cookieconsent_status", ""];
+
+    			//create array of cookies set
+    			var cookies = document.cookie.split(";");
+
+    			//loop through the cookies
+    			for (var i = 0; i < cookies.length; i++) {
+    				var cookie = cookies[i];
+
+    				//Get the cookie name
+    				var eqPos = cookie.indexOf("=");
+    				var name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+
+    				//Delete all cookies except those listed in essential
+    				if (essential === undefined || essential.length == 0 || essential.indexOf(name) == -1){
+    					document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    				}
+				}
+			};
+
+			window.cookieconsent.initialise({
+    		 	"palette": {
+    		    	"popup": {
+						"background": "#efeeee",
+						"text": "#333333"
+					},
+					"button": {
+						"background": "#ef4136",
+						"text": "#ffffff"
+    		    	}
+    		  	},
+			  	"type": "opt-out",
+			  	"content": {
+    				"message": 'This site uses cookies to track the number of visitors.',
+    				"link": 'Privacy Policy',
+					"dismiss": "Allow",
+					"allow": "Allow",
+					"deny": "Deny",
+					"href": "https://www.johnmackintosh.com/Privacy/",
+    		  	},
+
+			  	revokable:true,
+			  	animateRevokable:false,
+    			law: {
+    				regionalLaw: true,
+    			},
+    			location: true,
+    			onInitialise: function (status) {
+    				var type = this.options.type;
+					var didConsent = this.hasConsented();
+
+					if (type == 'opt-out' && didConsent) {
+
+    			    	// Google Tag Manager
+    			    	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-PL5CVNH');
+
+    			  	} else {
+    					this.deleteCookies();
+						// window.location.reload();
+    			  	}
+    			},
+
+    			onStatusChange: function(status, chosenBefore) {
+    			  	var type = this.options.type;
+				  	var didConsent = this.hasConsented();
+
+				  	if (type == 'opt-out' && didConsent) {
+
+				  		// Google Tag Manager
+    			    	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+						new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+						j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+						'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+						})(window,document,'script','dataLayer','GTM-PL5CVNH');
+
+    			  	} else {
+    					this.deleteCookies();
+						// window.location.reload();
+    			  	}
+    			},
+    		})
+		});
+
+	} else {
+
+		// Show informational cookie consent if user is from all other countries not part of the EU.
+
+    	window.addEventListener("load", function(){
+    		window.cookieconsent.initialise({
+    		 	"palette": {
+    		    	"popup": {
+						"background": "#efeeee",
+						"text": "#333333"
+    		    	},
+					"button": {
+						"background": "#ef4136",
+						"text": "#ffffff"
+    		    	}
+    		  	},
+			  	"content": {
+    				"message": 'This site uses cookies to track the number of visitors.',
+    				"link": 'Privacy Policy',
+					"dismiss": "OK",
+					"allow": "Accept",
+					"deny": "Deny",
+					"href": "",
+    		 	},
+    		})
+		});
+
+		// Google Tag Manager
+    	(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+		new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+		j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+		'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+		})(window,document,'script','dataLayer','GTM-PL5CVNH');
+	}
+}, "jsonp");
